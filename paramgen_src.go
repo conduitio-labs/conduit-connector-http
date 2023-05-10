0b21ef50c11e467388e88b01d528bc93f46d5d33
maha-hajja <maha.d.hajja@gmail.com>
2023-05-10 13:52:01 +0100

    add more params

--- paramgen_src.go
+++ paramgen_src.go
@@ -9,15 +9,41 @@ import (
 
 func (SourceConfig) Parameters() map[string]sdk.Parameter {
 	return map[string]sdk.Parameter{
+		"body": {
+			Default:     "",
+			Description: "Http body to use in the request",
+			Type:        sdk.ParameterTypeString,
+			Validations: []sdk.Validation{},
+		},
+		"headers": {
+			Default:     "",
+			Description: "Http headers to use in the request, comma separated list of : separated pairs",
+			Type:        sdk.ParameterTypeString,
+			Validations: []sdk.Validation{},
+		},
+		"method": {
+			Default:     "GET",
+			Description: "Http method to use in the request",
+			Type:        sdk.ParameterTypeString,
+			Validations: []sdk.Validation{
+				sdk.ValidationInclusion{List: []string{"GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "CONNECT", "OPTIONS", "TRACE"}},
+			},
+		},
+		"params": {
+			Default:     "",
+			Description: "parameters to use in the request, & separated list of = separated pairs",
+			Type:        sdk.ParameterTypeString,
+			Validations: []sdk.Validation{},
+		},
 		"pollingPeriod": {
 			Default:     "5m",
-			Description: "",
+			Description: "how often the connector will get data from the url",
 			Type:        sdk.ParameterTypeDuration,
 			Validations: []sdk.Validation{},
 		},
 		"url": {
 			Default:     "",
-			Description: "",
+			Description: "Http url to use in the request",
 			Type:        sdk.ParameterTypeString,
 			Validations: []sdk.Validation{
 				sdk.ValidationRequired{},
--- source.go
+++ source.go
@@ -37,31 +37,29 @@ type Source struct {
 }
 
 type SourceConfig struct {
-	URL           string        `json:"url" validate:"required"`
+	// Http url to use in the request
+	URL string `json:"url" validate:"required"`
+	// how often the connector will get data from the url
 	PollingPeriod time.Duration `json:"pollingPeriod" default:"5m"`
+	// Http method to use in the request
+	Method string `default:"GET" validate:"inclusion=GET|POST|PUT|DELETE|PATCH|HEAD|CONNECT|OPTIONS|TRACE"`
+	// Http headers to use in the request, comma separated list of : separated pairs
+	Headers string
+	// parameters to use in the request, & separated list of = separated pairs
+	Params string
+	// Http body to use in the request
+	Body string
 }
 
 func NewSource() sdk.Source {
-	// Create Source and wrap it in the default middleware.
 	return sdk.SourceWithMiddleware(&Source{})
 }
 
 func (s *Source) Parameters() map[string]sdk.Parameter {
-	// Parameters is a map of named Parameters that describe how to configure
-	// the Source. Parameters can be generated from SourceConfig with paramgen.
 	return s.config.Parameters()
 }
 
 func (s *Source) Configure(ctx context.Context, cfg map[string]string) error {
-	// Configure is the first function to be called in a connector. It provides
-	// the connector with the configuration that can be validated and stored.
-	// In case the configuration is not valid it should return an error.
-	// Testing if your connector can reach the configured data source should be
-	// done in Open, not in Configure.
-	// The SDK will validate the configuration and populate default values
-	// before calling Configure. If you need to do more complex validations you
-	// can do them manually here.
-
 	sdk.Logger(ctx).Info().Msg("Configuring Source...")
 	err := sdk.Util.ParseConfig(cfg, &s.config)
 	if err != nil {
@@ -71,13 +69,7 @@ func (s *Source) Configure(ctx context.Context, cfg map[string]string) error {
 }
 
 func (s *Source) Open(ctx context.Context, pos sdk.Position) error {
-	// Open is called after Configure to signal the plugin it can prepare to
-	// start producing records. If needed, the plugin should open connections in
-	// this function. The position parameter will contain the position of the
-	// last record that was successfully processed, Source should therefore
-	// start producing records after this position. The context passed to Open
-	// will be cancelled once the plugin receives a stop signal from Conduit.
-
+	// create client
 	s.client = &http.Client{}
 
 	// check connection
@@ -125,12 +117,11 @@ func (s *Source) Read(ctx context.Context) (sdk.Record, error) {
 		return sdk.Record{}, fmt.Errorf("error getting data: %w", err)
 	}
 	return rec, nil
-	return sdk.Record{}, nil
 }
 
 func (s *Source) getRecord(ctx context.Context) (sdk.Record, error) {
 	// create GET request
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, s.config.URL, nil)
+	req, err := http.NewRequestWithContext(ctx, s.config.Method, s.config.URL, nil)
 	if err != nil {
 		return sdk.Record{}, fmt.Errorf("error creating HTTP request: %w", err)
 	}
@@ -171,20 +162,11 @@ func (s *Source) getRecord(ctx context.Context) (sdk.Record, error) {
 }
 
 func (s *Source) Ack(ctx context.Context, position sdk.Position) error {
-	// Ack signals to the implementation that the record with the supplied
-	// position was successfully processed. This method might be called after
-	// the context of Read is already cancelled, since there might be
-	// outstanding acks that need to be delivered. When Teardown is called it is
-	// guaranteed there won't be any more calls to Ack.
-	// Ack can be called concurrently with Read.
+	sdk.Logger(ctx).Debug().Str("position", string(position)).Msg("got ack")
 	return nil
 }
 
 func (s *Source) Teardown(ctx context.Context) error {
-	// Teardown signals to the plugin that there will be no more calls to any
-	// other function. After Teardown returns, the plugin should be ready for a
-	// graceful shutdown.
-
 	if s.client != nil {
 		s.client.CloseIdleConnections()
 	}
