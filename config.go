c2569ec0cf030595f842e4dc5c145e6b429f51b5
maha-hajja <maha.d.hajja@gmail.com>
2023-05-11 13:30:27 +0100

    rename to discord

--- config.go
+++ config.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package http
+package discord
 
 //go:generate paramgen -output=paramgen_src.go SourceConfig
 
@@ -48,6 +48,9 @@ func (s SourceConfig) ParseConfig(cfg map[string]string) (SourceConfig, http.Hea
 	header, err := s.parseHeaders()
 	if err != nil {
 		return SourceConfig{}, nil, fmt.Errorf("invalid config: %w", err)
+	}
+	if s.Params != "" {
+		s.URL = s.URL + "?" + s.Params
 	}
 	return s, header, nil
 }
--- source.go
+++ source.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package http
+package discord
 
 //go:generate paramgen -output=paramgen_src.go SourceConfig
 
